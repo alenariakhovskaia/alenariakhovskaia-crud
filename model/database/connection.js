01f6792c8072c2421e373c0222ad0b769b4efbd7
alenariakhovskaia <98536084+alenariakhovskaia@users.noreply.github.com>
2022-12-12 11:01:09 -0500

    Update connection.js

--- model/database/connection.js
+++ model/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection 
-        const con = await mongoose.connect("mongodb+srv://alenariakhovskaia:PerXpD8PRblcgMgR@cluster0.ajpbio2.mongodb.net/?retryWrites=true&w=majority", { 
+        const con = await mongoose.connect("mongodb+srv://alenariakhovskaia:PerXpD8PRblcgMgR@cluster0.ajpbio2.mongodb.net/alenariakhovskaia?retryWrites=true&w=majority", { 
             useNewUrlParser: true, 
             useUnifiedTopology: true, 
             useCreateIndex: true, 
